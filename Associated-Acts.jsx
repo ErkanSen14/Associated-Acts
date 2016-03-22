7557883411e46d7f6cc54ee5be3e1154db7f9de4
Erkan Sen <Erkan Sen>
2016-03-22 13:02:27 -0500

    added different method of getting info

--- Associated-Acts.jsx
+++ Associated-Acts.jsx
@@ -1,4 +1,7 @@
 Rappers = new Mongo.Collection("rappers")
+var page='Tupac'
+var language='en'
+var rappers=null
 var texts
 if (Meteor.isClient) {
   Meteor.startup(function() {
@@ -9,7 +12,7 @@ if (Meteor.isClient) {
 
         $.ajax({
           type: "GET",
-          url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=J._Cole&callback=?",
+          url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Outkast&callback=?",
           contentType: "application/json; charset=utf-8",
           async: false,
           dataType: "json",
@@ -32,50 +35,74 @@ if (Meteor.isClient) {
 
             eachRecursive(blurb)
 
-            aa=texts
-            var index=aa.indexOf('Associated acts')
+            aa = texts
+            var index = aa.indexOf('Associated acts')
             var aa2
             for (var value of aa) {
               if (aa.indexOf('Associated acts') > -1) {
-                aa2=aa.substring(index).split(/[\r\n]+/g)
+                aa2 = aa.substring(index).split('\n')
 
               }
             }
-            console.log(aa2)
             var aa3 = []
-            for (var value of aa2)
-              if (value != "Associated acts")
-                if (value != "")
-                  aa3.push(value)
-
-          console.log(aa3)
-
+            for (var x = 0; x < aa2.length; x++)
+              if (aa2[x] != "Associated acts")
+                if (aa2[x] != "") {
+                  aa3.push(aa2[x])
+                  if (aa2[x + 1] == "")
+                    break
+                }
+              console.log(aa3)
+              return yolo()
           },
           error: function(errorMessage) {}
         });
       })
     }
-
-    function eachRecursive(obj)
-    {
+    function eachRecursive(obj) {
       for (var k in obj) {
-                if (typeof obj[k] == "object" && obj[k] !== null)
-                  eachRecursive(obj[k]);
-                else if(typeof obj[k]=="string"&& obj[k].indexOf("Associated acts")>-1)
-                {console.log(obj[k])
-                  texts=obj[k]
-                break;
-                }
+        if (typeof obj[k] == "string" && obj[k].indexOf("Associated acts") > -1) {
+          texts = obj[k]
+          break;}
+        else if (typeof obj[k] == "object" && obj[k] !== null)
+          eachRecursive(obj[k]);
+
+        }
+
+      }
 
 
-    }}
+    getWiki(title)
+    function yolo(){
+    Meteor.call("getWikiStuff",function(){
 
-getWiki(title)
+        console.log(rappers)
+    })
+
+}
+
   })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function() {
-    // code to run on server at startup
+    Meteor.methods({
+      getWikiStuff: function(){
+        var infobox=Meteor.npmRequire('wiki-infobox')
+
+
+
+        infobox(page,language,function(err,data){
+          if (err){return}
+          rappers=data.associated_acts
+          return rappers
+
+      })}
+
+
+    });
+
+
+
   });
 }
