a8e13a9a92f2b61144bf02b3f52f1907e7de72ec
Erkan Sen <erkansen14@hotmail.com>
2016-06-03 18:58:09 -0500

    using working example

--- Associated-Acts.jsx
+++ Associated-Acts.jsx
@@ -1,32 +1,34 @@
 Rappers = new Mongo.Collection(null)
 
-var rappers = null
-var texts
-var counter = 0
-var rapperInsert
+let rappers = null
+let texts
+let counter = 0
+let rapperInsert
+let index = 0
 listOfDefaults = ["Tupac", "Destiny's_Child", "Drake", "Wale"]
 added = []
-let dataset = []
-let names = []
+let names = {"nodes":[{"name":"Myriel","group":1},{"name":"Napoleon","group":1},{"name":"Mlle.Baptistine","group":1},{"name":"Mme.Magloire","group":1}]}
+let links = {"links":[{"source":1,"target":0,"value":1},{"source":2,"target":0,"value":8},{"source":3,"target":0,"value":10},]}
+
 if (Meteor.isClient) {
   Meteor.startup(function() {
-    var rappers = []
+  /*  let rappers = []
     //React.render(<App/>, document.getElementById('bar'))
-    var title = "J._Cole"
+    let title = "J._Cole"
     function getWikiForreal(wikiTitle) {
       rappers = []
       if (!Rappers.findOne({name: wikiTitle})) {
         added.push(wikiTitle)
-        var url = "http://dbpedia.org/sparql";
-        var query = "select ?z where {<http://dbpedia.org/resource/" + wikiTitle + "> dbo:associatedMusicalArtist ?z}"
-        var queryUrl = encodeURI(url + "?query=" + query + "&format=json");
-        var temp
+        let url = "http://dbpedia.org/sparql";
+        let query = "select ?z where {<http://dbpedia.org/resource/" + wikiTitle + "> dbo:associatedMusicalArtist ?z}"
+        let queryUrl = encodeURI(url + "?query=" + query + "&format=json");
+        let temp
         $.ajax({
           dataType: "jsonp",
           url: queryUrl,
           success: function(data) {
 
-            for (var value of data.results.bindings)
+            for (let value of data.results.bindings)
               rappers.push(value.z.value.split("http://dbpedia.org/resource/")[1])
 
 
@@ -43,8 +45,7 @@ if (Meteor.isClient) {
 
 
     function insertRappers(name, rappers,data){
-      dataset.push(rappers.length)
-      names.push(name)
+    //  names.nodes.push(name)
       if(rappers.length>0)
       rapperInsert = {
         name: name,
@@ -65,40 +66,58 @@ if (Meteor.isClient) {
     }
 
     function recurse(data){
-      for (var value of data.results.bindings) {
+      for (let value of data.results.bindings) {
         temp = value.z.value.split("http://dbpedia.org/resource/")[1]
-        //d3 function to draw
-      let svgContainer =  d3.select("body").selectAll("svg")
-                            .data(dataset)
-                            .enter()
-                            .append("svg")
-                            .attr("class", "bar")
-                            .style("height", function(d) {
-                              const barHeight = d * 5
-                              return barHeight + "px"
-                            })
-
-      let text = svgContainer.selectAll("text")
-                             .data(names)
-                             .enter()
-                             .append("text")
-      let textLabels = text
-                           .attr("x", function(d) { return 10; })
-                           .attr("y", function(d) { return 37.5; })
-                           .text(function(d) {return d})
-                           .attr("font-family", "sans-serif")
-                           .attr("font-size", "20px")
-                           .attr("fill", "black");
-
-
         getWikiForreal(temp)
 
-      }
+}
+}
 
 
-    }
 
-    getWikiForreal(title)
+    getWikiForreal(title)*/
+    //d3 function to draw
+    var width = 960,
+height = 500
+    var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+var force = d3.layout.force()
+  .gravity(0.05)
+  .distance(100)
+  .charge(-100)
+  .size([width, height]);
+force
+    .nodes(names.nodes)
+    .links(links.links)
+    .start();
+var link = svg.selectAll(".link")
+    .data(links.links)
+  .enter().append("line")
+    .attr("class", "link");
+var node = svg.selectAll(".node")
+    .data(names.nodes)
+  .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag);
+node.append("image")
+    .attr("xlink:href", "https://github.com/favicon.ico")
+    .attr("x", -8)
+    .attr("y", -8)
+    .attr("width", 16)
+    .attr("height", 16);
+node.append("text")
+    .attr("dx", 12)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.name });
+force.on("tick", function() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+})
+
   })
 }
 
